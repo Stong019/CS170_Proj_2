2e7ce28a96fdb3c62fa8b94ac2b1403d64d9ea5c
CodySawa <kota418@gmail.com>
2024-06-07 00:06:02 -0700

    normalize data

--- part2.py
+++ part2.py
@@ -1,5 +1,11 @@
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
-data = np.loadtxt('/content/small-test-dataset-1.txt')
-labels = data[:,0]
-print(labels)
+data = np.loadtxt('/CS170_Proj_2/small-test-dataset-1.txt')
+columns_to_normalize = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+scaler = MinMaxScaler()
+
+data[:, columns_to_normalize] = scaler.fit_transform(data[:, columns_to_normalize])
+
+print(data)
