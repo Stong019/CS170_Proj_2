3c2aff8188bd99a9fd66b3a677c9272094e2da58
VibhuSVK <45452967+VibhuSVK@users.noreply.github.com>
2024-06-06 23:04:59 -0700

    Update feat_select.py
    added backward elimination

--- feat_select.py
+++ feat_select.py
@@ -1,8 +1,6 @@
 import math
 import random
 
-
-
 def ForwardSelection(num_features):
     selectedFeatures = []
     best_accuracy = -1
@@ -27,7 +25,30 @@ def ForwardSelection(num_features):
     print(f"Finished search!!! the best feature subset is {selectedFeatures}, which has an accuracy of {best_accuracy:.4f}\n")
 
 def BackwardElimination(num_features):
-    return
+    selectedFeatures = list(range(1, num_features + 1))
+    best_accuracy = -1
+    print("\nBeginning Search")
+    
+    accuracy = random.uniform(0, 1)
+    best_accuracy = accuracy
+    print(f"Using Features{selectedFeatures}, accuracy is {accuracy:.4f}")
+
+    for i in range(num_features, 0, -1):
+        bestFeature = None
+        for currentFeature in selectedFeatures:
+            current_set = [feature for feature in selectedFeatures if feature != currentFeature]
+            accuracy = random.uniform(0, 1)
+            print(f"Using Features{current_set}, accuracy is {accuracy:.4f}")
+            if accuracy > best_accuracy:
+                best_accuracy = accuracy
+                bestFeature = currentFeature
+        if bestFeature:
+            selectedFeatures.remove(bestFeature)
+            print(f"Feature set {selectedFeatures} was best, accuracy is {best_accuracy:.4f}")
+        else:
+            break
+    
+    print(f"Finished search!!! the best feature subset is {selectedFeatures}, which has an accuracy of {best_accuracy:.4f}\n")
 
 def main():
     print('Welcome to Group 61\'s Feature Selection Algorithm.')
